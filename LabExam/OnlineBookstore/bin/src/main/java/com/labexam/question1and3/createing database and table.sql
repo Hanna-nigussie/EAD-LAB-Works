7a6beb63850d70b11e995899671f4f9d05bf2aef
hannanigussie <hniguse95@gmail.com>
2025-02-01 00:41:08 +0300

    service scheduling system

--- LabExam/OnlineBookstore/bin/src/main/java/com/labexam/question1and3/createing database and table.sql
+++
@@ -1,8 +0,0 @@
-create database tododb;
-use tododb;
-create table tasks (
-	id int auto_increment primary key, 
-	title varchar(255), 
-	author VARCHAR(255), 
-	price double
-);
--- LabExam/OnlineBookstore/src/main/java/com/question1and3/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE BookstoreDB;
-USE BookstoreDB;
-CREATE TABLE Books (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
-    title VARCHAR(255), 
-    author VARCHAR(255), 
-    price DOUBLE
-);
